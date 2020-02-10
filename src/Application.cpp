d4ca0e0e6726ca4b6107f774ae5546cd84e49994
Jihyo Kim <kimjihyo0325@gmail.com>
2020-02-09 21:31:10 -0800

    now user can place stones

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <cmath>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+sf::CircleShape CreateStone(const sf::Vector2f &position, int type)
+{
+    float stoneSize = 40.f;
+    float stoneOutlineThickness = 4.f;
+    sf::CircleShape stone(stoneSize);
+    if (type == 1)
+    {
+        stone.setFillColor(sf::Color::Black);
+    }
+    else
+    {
+        stone.setFillColor(sf::Color::White);
+    }
+    stone.setPosition(position);
+    stone.setOutlineThickness(stoneOutlineThickness);
+    stone.setOutlineColor(sf::Color::Black);
+    return stone;
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1920), "Gomoku");
-    window.setFramerateLimit(5);
+    sf::RenderWindow window(sf::VideoMode(1920, 1920), "Gomoku", sf::Style::Close);
+    window.setFramerateLimit(20);
     sf::Color backgroundColor(245, 240, 225);
     sf::Vector2u windowSize = window.getSize();
     sf::Vector2f boardSize(1200.f, 1200.f);
@@ -37,30 +57,61 @@ int main()
     }
 
     float stoneSize = 40.f;
-    float stoneOutlineThickness = 4.f;
-    sf::CircleShape stone(stoneSize);
-    stone.setFillColor(sf::Color::Black);
-    stone.setPosition(sf::Vector2f(boardPosition.x - stoneSize, boardPosition.y - stoneSize));
-    stone.setOutlineThickness(stoneOutlineThickness);
-    stone.setOutlineColor(sf::Color::Black);
+    int grid[15][15];
+    sf::CircleShape *stoneBuffer[15][15];
 
-    sf::CircleShape stone2(stoneSize);
-    stone2.setFillColor(sf::Color::Black);
-    stone2.setPosition(sf::Vector2f(boardPosition.x + spacing - stoneSize, boardPosition.y - stoneSize));
-    stone2.setOutlineThickness(stoneOutlineThickness);
-    stone2.setOutlineColor(sf::Color::Black);
+    for (int y = 0; y < 15; y++)
+    {
+        for (int x = 0; x < 15; x++)
+        {
+            stoneBuffer[y][x] = nullptr;
+            grid[y][x] = 0;
+        }
+    }
 
-    sf::CircleShape stone3(stoneSize);
-    stone3.setFillColor(sf::Color::White);
-    stone3.setPosition(sf::Vector2f(boardPosition.x + spacing * 2 - stoneSize, boardPosition.y - stoneSize));
-    stone3.setOutlineThickness(stoneOutlineThickness);
-    stone3.setOutlineColor(sf::Color::Black);
+    int counter = 0;
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            if (event.mouseButton.button == sf::Mouse::Left && event.type == sf::Event::MouseButtonReleased)
+            {
+                sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+                float predictedXPos = (localPosition.x - board.getPosition().x) / spacing;
+                float predictedYPos = (localPosition.y - board.getPosition().y) / spacing;
+                int indexX = roundf(predictedXPos);
+                int indexY = roundf(predictedYPos);
+                float positionToPlaceX = indexX * spacing + boardPosition.x - stoneSize;
+                float positionToPlaceY = indexY * spacing + boardPosition.y - stoneSize;
+
+                std::cout << "(index) x: " << indexX << " y: " << indexY << std::endl;
+                std::cout << "(position) x: " << positionToPlaceX << " y: " << positionToPlaceY << std::endl;
+                std::cout << "(mouse) x: " << localPosition.x << " y: " << localPosition.y << std::endl;
+
+                std::cout << "grid[indexY][indexX]: " << grid[indexY][indexX] << std::endl;
+                if (stoneBuffer[indexY][indexY] == nullptr)
+                {
+                    std::cout << "stoneBuffer[indexY][indexY]: "
+                              << "Not Placed" << std::endl;
+                }
+                else
+                {
+                    std::cout << "stoneBuffer[indexY][indexY]: "
+                              << "Placed" << std::endl;
+                }
+
+                if (grid[indexY][indexX] == 0 && stoneBuffer[indexY][indexX] == nullptr)
+                {
+                    if (indexY > -1 && indexY < 15 && indexX > -1 && indexX < 15)
+                    {
+                        std::cout << "Stone placed!" << std::endl;
+                        grid[indexY][indexX] = ++counter;
+                        stoneBuffer[indexY][indexX] = new sf::CircleShape(CreateStone(sf::Vector2f(positionToPlaceX, positionToPlaceY), counter % 2));
+                    }
+                }
+            }
             if (event.type == sf::Event::Closed)
             {
                 window.close();
@@ -72,10 +123,30 @@ int main()
         {
             window.draw(lines[i]);
         }
-        window.draw(stone);
-        window.draw(stone2);
-        window.draw(stone3);
+        for (int y = 0; y < 15; y++)
+        {
+            for (int x = 0; x < 15; x++)
+            {
+                if (stoneBuffer[y][x] != nullptr)
+                {
+                    window.draw(*stoneBuffer[y][x]);
+                }
+            }
+        }
+
         window.display();
     }
+
+    for (int y = 0; y < 15; y++)
+    {
+        for (int x = 0; x < 15; x++)
+        {
+            if (stoneBuffer[y][x] != nullptr)
+            {
+                delete stoneBuffer[y][x];
+            }
+        }
+    }
+
     return 0;
 }
