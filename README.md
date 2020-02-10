bdc42e03ae22842e9503110d8f8101bcc78d8212
Jihyo Kim <kimjihyo0325@gmail.com>
2020-02-09 18:59:46 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gomoku
+
+### How to build the project
+1. Create a build folder 
+```
+mkdir build
+```
+2. Go into the build folder
+```
+cd build
+```
+3. Setup CMake in the build folder
+```
+cmake ..
+```
+4. Compile and link the project with CMake
+```
+cmake --build .
+```
+5. Run the Gomoku executable
+```
+./Gomoku
+```
---
+++ src/Application.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Gomoku");
+    window.setFramerateLimit(10);
+
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    return 0;
+}
