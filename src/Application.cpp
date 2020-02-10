1f0b513840ee571dd00cc67e8a029305d215628e
Jihyo Kim <kimjihyo0325@gmail.com>
2020-02-09 20:21:11 -0800

    changed colors and added some stones

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,14 +5,15 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920, 1920), "Gomoku");
-    window.setFramerateLimit(10);
+    window.setFramerateLimit(5);
     sf::Color backgroundColor(245, 240, 225);
     sf::Vector2u windowSize = window.getSize();
     sf::Vector2f boardSize(1200.f, 1200.f);
     sf::Vector2f boardPosition(windowSize.x / 2 - boardSize.x / 2, windowSize.y / 2 - boardSize.y / 2);
 
+    sf::Color boardColor(202, 164, 114);
     sf::RectangleShape board(boardSize);
-    board.setFillColor(sf::Color(222, 218, 206));
+    board.setFillColor(boardColor);
     board.setPosition(boardPosition);
 
     // Draw 30 lines (15 for horizontal, 15 for vertical)
@@ -35,7 +36,25 @@ int main()
         lines[i].setFillColor(sf::Color::Black);
     }
 
-    window.draw(board);
+    float stoneSize = 40.f;
+    float stoneOutlineThickness = 4.f;
+    sf::CircleShape stone(stoneSize);
+    stone.setFillColor(sf::Color::Black);
+    stone.setPosition(sf::Vector2f(boardPosition.x - stoneSize, boardPosition.y - stoneSize));
+    stone.setOutlineThickness(stoneOutlineThickness);
+    stone.setOutlineColor(sf::Color::Black);
+
+    sf::CircleShape stone2(stoneSize);
+    stone2.setFillColor(sf::Color::Black);
+    stone2.setPosition(sf::Vector2f(boardPosition.x + spacing - stoneSize, boardPosition.y - stoneSize));
+    stone2.setOutlineThickness(stoneOutlineThickness);
+    stone2.setOutlineColor(sf::Color::Black);
+
+    sf::CircleShape stone3(stoneSize);
+    stone3.setFillColor(sf::Color::White);
+    stone3.setPosition(sf::Vector2f(boardPosition.x + spacing * 2 - stoneSize, boardPosition.y - stoneSize));
+    stone3.setOutlineThickness(stoneOutlineThickness);
+    stone3.setOutlineColor(sf::Color::Black);
 
     while (window.isOpen())
     {
@@ -53,6 +72,9 @@ int main()
         {
             window.draw(lines[i]);
         }
+        window.draw(stone);
+        window.draw(stone2);
+        window.draw(stone3);
         window.display();
     }
     return 0;
